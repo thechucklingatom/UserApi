b64d6c8dd73fcd8636129bf9318794cacc153239
atom <thechucklingatom@gmail.com>
2023-09-04 10:44:21 -0600

    add get delete and post

--- userApi/Controllers/UsersController.cs
+++ userApi/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UserApi.Models;
 
@@ -9,8 +10,34 @@ public class UsersController: ControllerBase
 {
     [HttpGet]
     [ProducesResponseType(typeof(User), 200)]
-    public List<User> Get([FromServices] List<User> user)
+    public List<User> GetUsers([FromServices] List<User> users)
     {
-        return user;
+        return users;
     }
+
+    [HttpPost]
+    public ObjectResult AddUser(User user, [FromServices] List<User> users)
+    {
+        if (users.Exists(u => u.Email == user.Email))
+        {
+            return Ok("user already added");
+        }
+        
+        users.Add(user);
+        return Ok("user added to list");
+    }
+
+    [HttpDelete]
+    [Authorize]
+    public ObjectResult DeleteUser(string email, [FromServices] List<User> users)
+    {
+        if (users.Exists(user => user.Email == email))
+        {
+            users.RemoveAll(user => user.Email == email);
+            return Ok("deleted user");
+        }
+
+        return Ok("no user to delete");
+    }
+    
 }
